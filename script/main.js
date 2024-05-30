8a957299a715080def7a7649408ada6df699066f
chatlapin <choh9323@gmail.com>
2024-05-30 22:11:10 +0200

    30may on a fait bc

--- script/main.js
+++ script/main.js
@@ -1,19 +1,59 @@
-// 컨테이너
-const container = document.querySelector(".container");
+let currentRecipes = [];
 
-// recipesDatat 배열의 각 요소별 루프 처리
-recipesData.forEach((recipesData) => {
+async function getInitialRecipes() {
+  const response = await fetch("./assets/data/recipes.json");
+  const recipes = await response.json();
+  return recipes;
+}
 
-  // 각 요소 데이터를 쓰기
-  container.innerHTML += `
+async function init() {
+  currentRecipes = await getInitialRecipes();
+  //console.log("currentRecipes");
+  //console.log(currentRecipes);
+  displayRecipes();
+}
+
+init();
+
+const inputSearch = document.querySelector("#search-input");
+inputSearch.addEventListener("input", searchRecipes);
+function searchRecipes(event) {
+  const searchValue = event.target.value.toLowerCase();
+  if (searchValue.length < 3) {
+    return;
+  }
+  console.log("searchValue");
+  console.log(searchValue);
+  currentRecipes = currentRecipes.filter(
+    (recipe) =>
+      recipe.name.toLowerCase().includes(searchValue) ||
+      recipe.description.toLowerCase().includes(searchValue) ||
+      recipe.ingredients.some((ingredient) =>
+        ingredient.ingredient.toLowerCase().includes(searchValue)
+      )
+  );
+  displayRecipes();
+}
+
+function displayRecipes() {
+  const counterResults = document.querySelector("#counterResults");
+  counterResults.innerText = currentRecipes.length;
+
+  // 컨테이너
+  const container = document.querySelector(".container");
+  container.innerHTML = "";
+  // recipesDatat 배열의 각 요소별 루프 처리
+  currentRecipes.forEach((recipeData) => {
+    // 각 요소 데이터를 쓰기
+    container.innerHTML += `
         <div class="card">
-          <img src="assets/photos/${recipesData.image}" alt="${recipesData.name}">
-          <h2>${recipesData.name}</h2>
+          <img src="assets/photos/${recipeData.image}" alt="${recipeData.name}">
+          <h2>${recipeData.name}</h2>
           <h3>RECETTE</h3>
-          <p>${recipesData.description}</p>
+          <p>${recipeData.description}</p>
           <h3>INGRÉDIENTS</h3>
           <ul>
-            ${recipesData.ingredients
+            ${recipeData.ingredients
               .map(
                 (ingredient) => `
                 <li>${ingredient.ingredient} : ${
@@ -27,16 +67,18 @@ recipesData.forEach((recipesData) => {
           </ul>
         </div>
   `;
-});
-
+  });
+}
+/*
 // Function to update displayed cards
 function updateCards(recipes) {
-  container.innerHTML = '';
+  container.innerHTML = "";
   recipes.forEach((recipe, index) => {
     container.innerHTML += generateRecipeHTML(recipe, images[index]);
   });
-}
-
+}}
+*/
+/*
 const filterButtons = document.querySelectorAll(".filters button");
 
 filterButtons.forEach((button) => {
@@ -44,3 +86,4 @@ filterButtons.forEach((button) => {
     button.classList.toggle("active"); // Toggle "active" class on click
   });
 });
+*/
